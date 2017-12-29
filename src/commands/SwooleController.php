58861a7c2641a227f132a9b50c612b6b3446b54a
张梦林 <dream_zml@163.com>
2017-12-29 12:33:45 +0800

    rpc增加  start stop restart reload方法

--- src/commands/SwooleController.php
+++ src/commands/SwooleController.php
@@ -7,9 +7,26 @@ use tourze\swoole\yii2\server\RpcServer;
 use Yii;
 use yii\console\Controller;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Console;
 
 class SwooleController extends Controller
 {
+    /**
+     * @var 端口
+     */
+    public $port;
+
+    /**
+     * @var 帮助
+     */
+    public $helpHand;
+
+    public $rpcCommonds = ['start'=>'start rpc service (default)',
+                           'reload'=>'reload rpc service',
+                           'stop'=>'shutdown rpc service',
+                           'restart'=>'restart rpc service',
+                        ];
+
 
     /**
      * Run swoole http server
@@ -25,25 +42,236 @@ class SwooleController extends Controller
     }
     
     /**
-     * Run swoole rpc server
-     * 不能一次性起多个服务
+     * swoole rpc server
+     * @desp 一次只能开启一个服务
      *
-     * @param string $app Running app
+     * @param string $app service name
+     * @param string $commond cmd
+     * @param string $port service port
      *
      * @throws \yii\base\InvalidConfigException
      */
-    public function actionRpc($app)
+    public function actionRpc($app=null, $commond='start')
     {
         $config = Yii::$app->params['swooleServer'];
         $baseRoot = ArrayHelper::remove($config, 'baseRoot');
-        
-        if(!in_array($app, $config['rpcList'])){
-            echo "rpc service: {$app} not exists";
-            return 1;
+
+        $service = scandir($baseRoot);
+        $service = array_diff($service, ['.', '..']);
+        sort($service);
+
+        if(array_key_exists($app, $this->rpcCommonds)){
+            $commond = $app;
+            $app = null;
+        }
+        if(!$app || !in_array($app, $service) || $this->helpHand){
+            $this->rpcHelp($baseRoot, $service);
+
+            //帮助
+            if($this->helpHand){
+                return;
+            }
+        }
+
+        //选择应用
+        while (!(in_array($app, $service))) {
+            $appId = $this->select("plase select $commond rpcService\n", $service);
+            $app   = isset($service[$appId]) ? $service[$appId] : '';
         }
 
         $conf = require($baseRoot.DIRECTORY_SEPARATOR.$app.DIRECTORY_SEPARATOR.'config/swoole.service.php');
+        $appConf = require($baseRoot.DIRECTORY_SEPARATOR.$app.DIRECTORY_SEPARATOR.'config/yii.application.php');
+
+        if($this->port){
+            $conf['port'] = $this->port;
+        }
+
         $server = new RpcServer;
-        $server->run($conf);
+        $server->pidFile = "/tmp/rpc_{$conf['port']}.pid";
+
+        //关闭服务
+        if($commond=='stop'){
+            if(!$server->isRunning()){
+                $this->resOut("stop $app warning, port: {$conf['port']} not runing", Console::FG_YELLOW);
+                return 1;
+            }
+
+
+            $res = $server->stop();
+            if($res){
+                $this->resOut("stop $app success",  Console::FG_GREEN);
+            }else{
+                $this->resOut( "stop $app fail, Please try again later",  Console::FG_RED);
+            }
+            return $res?0:1;
+        }
+
+        //开启服务
+        if($commond=='start'){
+            if($server->isRunning() && $conf['server']['daemonize']){
+                $this->resOut("start $app warning, port: {$conf['port']} has been occupied, plase sure service is already runing", Console::FG_YELLOW);
+                return 1;
+            }
+
+            $this->resOut("start $app success, port: {$conf['port']}, name: {$appConf['name']}", Console::FG_GREEN);
+            $server->run($conf);
+            return 1;
+        }
+
+        //重新启动服务
+        if($commond=='restart'){
+            $this->stdout("                    Information Panel                    \n", Console::FG_GREEN);
+            $this->stdout("*********************************************************\n", Console::FG_GREEN);
+
+            if($server->isRunning()){
+                $server->stop();
+                $this->stdout("stop $app success, port: {$conf['port']}\n",  Console::FG_GREEN);
+            }else{
+                $this->stdout("stop $app warning, port: {$conf['port']} not runing\n", Console::FG_YELLOW);
+            }
+            $this->stdout("start $app success, port: {$conf['port']}, name: {$appConf['name']}\n", Console::FG_GREEN);
+            $this->stdout("*********************************************************\n", Console::FG_GREEN);
+            $server->run($conf);
+            return 1;
+        }
+
+        if($commond=='reload'){
+            if(!$server->isRunning()){
+                $this->resOut("reload $app error, port: {$conf['port']}  is not runing", Console::FG_RED);
+                return 1;
+            }
+
+            $res = $server->reload();
+            if($res){
+                $this->resOut("reload $app success, port: {$conf['port']}, name: {$appConf['name']}", Console::FG_GREEN);
+            }else{
+                $this->resOut("reload $app fail, port: {$conf['port']}, name: {$appConf['name']}", Console::FG_RED);
+            }
+        }
+
+        //$this->stdout(" service:$app start success\n", Console::FG_GREEN);
+    }
+
+
+    /**
+     * 输出
+     * @param     $text
+     * @param int $args
+     */
+    public function resOut($text,$args=Console::FG_YELLOW){
+        //$args = func_get_args();
+        //array_shift($args);
+        $this->stdout("                    Information Panel                    \n", $args);
+        $this->stdout("*********************************************************\n", $args);
+        $this->stdout("$text\n", $args);
+        $this->stdout("*********************************************************\n", $args);
+    }
+
+
+
+    /**
+     * 提示rpc帮助
+     * @param $baseRoot
+     * @param $service
+     */
+    private function rpcHelp($baseRoot, $service){
+        $len = 30;
+        $this->stdout($this->ansiFormat("Used\n", Console::FG_YELLOW));
+        $this->stdout('    ' .$this->ansiFormat("php yii swoole/rpc [RpcService|Command] [Command] [-Options optionValue ...] \n",  Console::FG_PURPLE.';'.Console::BOLD));
+
+        $this->stdout("\n");
+        $this->stdout($this->ansiFormat("RpcServiceLists\n", Console::FG_YELLOW));
+        foreach ($service as $key=>$ser){
+            $this->stdout('    ' .$this->ansiFormat("$key, $ser", Console::FG_GREEN));
+            $this->stdout(str_repeat(' ', $len + 4 - strlen($ser)));
+            $appConfigFile = $baseRoot.DIRECTORY_SEPARATOR.$ser.DIRECTORY_SEPARATOR.'config/yii.application.php';
+            $rpcConfigFile = $baseRoot.DIRECTORY_SEPARATOR.$ser.DIRECTORY_SEPARATOR.'config/swoole.service.php';
+            if(!file_exists($appConfigFile) || !file_exists($rpcConfigFile) ){
+                $this->stdout(Console::wrapText("config is not available", $len + 4 + 2));
+            }else{
+                $yiiConf = require($appConfigFile);
+                $swooleConf = require($rpcConfigFile);
+                $this->stdout($yiiConf['name']);
+                $this->stdout(str_repeat(' ', $len - mb_strlen($yiiConf['name'])*2));
+                $this->stdout('default port:'.$swooleConf['port'], Console::FG_GREY);
+            }
+            $this->stdout("\n");
+        }
+
+        $this->stdout("\n");
+        $this->stdout($this->ansiFormat("Commands\n", Console::FG_YELLOW));
+        $commonds = $this->rpcCommonds;
+        foreach ($commonds as $com=>$desp){
+            $this->stdout('    ' .$this->ansiFormat($com, Console::FG_GREEN));
+            $this->stdout(str_repeat(' ', $len + 4 - strlen($com)));
+            $this->stdout(Console::wrapText($desp, $len + 4 + 2));
+            $this->stdout("\n");
+        }
+
+        $this->stdout("\n");
+        $this->stdout($this->ansiFormat("Options\n", Console::FG_YELLOW));
+        $commonds = ['-p'=>'swoole service port', '-h'=> 'show help'];
+        foreach ($commonds as $com=>$desp) {
+            $this->stdout('    '.$this->ansiFormat($com, Console::FG_GREEN));
+            $this->stdout(str_repeat(' ', $len + 4 - strlen($com)));
+            $this->stdout(Console::wrapText($desp, $len + 4 + 2));
+            $this->stdout("\n");
+        }
+
+        $this->stdout("\n\n");
+    }
+
+
+
+    /**
+     * 服务是否已启动
+     *
+     * @return bool
+     */
+    public function isRunning($port)
+    {
+        $masterIsLive = false;
+        $pFile = "/tmp/swoft.pid";
+
+        // pid 文件是否存在
+        if (file_exists($pFile)) {
+            // 文件内容解析
+            $pidFile = file_get_contents($pFile);
+            $pids = explode(',', $pidFile);
+
+            $this->serverSetting['masterPid'] = $pids[0];
+            $this->serverSetting['managerPid'] = $pids[1];
+            $masterIsLive = $this->serverSetting['masterPid'] && @posix_kill($this->serverSetting['managerPid'], 0);
+        }
+
+        return $masterIsLive;
+    }
+
+    /**
+     * 动态参数
+     * @param string $actionID
+     *
+     * @return array|\string[]
+     */
+    public function options($actionID)
+    {
+        $parentOptions = parent::options($actionID);
+        if($actionID=='rpc'){
+            $parentOptions = ['port', 'helpHand'];
+        }
+
+        return $parentOptions;
+    }
+
+    /**
+     * 参数别名
+     * @return array
+     */
+    public function optionAliases()
+    {
+        return array_merge(parent::optionAliases(), [
+            'p' => 'port',
+            'h' => 'helpHand',
+        ]);
     }
 }
--- src/server/RpcServer.php
+++ src/server/RpcServer.php
@@ -23,7 +23,9 @@ use api\components\utils\YMLog;
  */
 class RpcServer extends HttpServer
 {
-    
+    public      $errMsg;
+    protected   $masterPid;
+
     /**
      * @inheritdoc
      */
@@ -247,5 +249,80 @@ class RpcServer extends HttpServer
             echo "http://127.0.0.1/xhprof/xhprof_html/index.php?run=" . $runId . '&source=xhprof_test'."\n";
         }
     }
-    
+
+    /**
+     * @param swoole_server $server
+     */
+    public function onServerStart($server)
+    {
+        parent::onServerStart($server);
+        if ($this->pidFile)
+        {
+            file_put_contents($this->pidFile, ',' . $server->manager_pid, FILE_APPEND);
+        }
+    }
+
+    /**
+     * 服务是否已启动
+     *
+     * @return bool
+     */
+    public function isRunning()
+    {
+        $masterIsLive = false;
+        $pFile = $this->pidFile;
+
+        // pid 文件是否存在
+        if (file_exists($pFile)) {
+            // 文件内容解析
+            $pidFile = file_get_contents($pFile);
+            $pids = explode(',', $pidFile);
+
+            $this->masterPid = $pids[0];
+            //$this->managerPid = $pids[1];
+
+            $masterIsLive = $this->masterPid && @posix_kill($this->masterPid, 0);
+        }
+
+        return $masterIsLive;
+    }
+
+    /**
+     * stop服务
+     */
+    public function stop()
+    {
+        $timeout = 60;
+        $startTime = time();
+        $this->masterPid && posix_kill($this->masterPid, SIGTERM);
+
+        $result = true;
+        while (1) {
+            $masterIslive = $this->masterPid && posix_kill($this->masterPid, SIGTERM);
+            if ($masterIslive) {
+                if (time() - $startTime >= $timeout) {
+                    $result = false;
+                    break;
+                }
+                usleep(10000);
+                continue;
+            }
+
+            break;
+        }
+        return $result;
+    }
+
+    /**
+     * reload服务
+     *
+     * @param bool $onlyTask 是否只重载任务
+     */
+    public function reload($onlyTask = false)
+    {
+        $signal = $onlyTask ? SIGUSR2 : SIGUSR1;
+
+        return posix_kill($this->masterPid, $signal);
+    }
+
 }
